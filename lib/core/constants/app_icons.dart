b793dc1eb95e034f56181b6b99817c406f4f2466
saqib-synvex <saqib.ali@synvex.io>
2026-07-27 13:03:38 +0500

    forgot pass set

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class AppIcons {
   static const IconData personIcon = Icons.person;
+  static const IconData mailIcon = Icons.mail_outlined;
 
   static const String backIcon = 'assets/svg_images/back_icon.svg';
   static const String vert_more = 'assets/svg_images/vert_more.svg';
