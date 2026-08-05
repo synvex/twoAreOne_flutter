b9f2217366e1f67f22f3fa634ec6a706d6eeb2ac
Sarfraz Afzal <sarfrazlqp03@gmail.com>
2026-08-05 16:20:18 +0500

    report converted from http to dio

--- lib/data/viewmodels/report_viewmodel.dart
+++ lib/data/viewmodels/report_viewmodel.dart
@@ -1,42 +1,55 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ReportViewModel with ChangeNotifier {
   String message = '';
   bool isLoading = false;
 
+  final Dio _dio = Dio();
+
   Future<bool> submitReport(int reportId, String reason, String comment) async {
     try {
       isLoading = true;
       notifyListeners();
+
       final prefs = await SharedPreferences.getInstance();
       final token = prefs.getString('auth_token') ?? '';
 
       print('Token: $token');
 
-      final response = await http.post(
-        Uri.parse('https://twoareone.love/api/user/report_user.php'),
-        headers: {'x-api-key': token},
-        body: jsonEncode({
+      final response = await _dio.post(
+        'https://twoareone.love/api/user/report_user.php',
+        data: {
           'reported_user_id': reportId.toString(),
           'reason': reason,
           'comment': comment,
-        }),
+        },
+        options: Options(headers: {'x-api-key': token}),
       );
 
       print("Status Code: ${response.statusCode}");
-      print("Response: ${response.body}");
+      print("Response: ${response.data}");
 
-      final data = jsonDecode(response.body);
+      final data = response.data;
 
       message = data['message'] ?? 'Unknown response';
 
       notifyListeners();
 
       return response.statusCode == 200;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print("Status Code: ${e.response?.statusCode}");
+        print("Response: ${e.response?.data}");
+
+        message = e.response?.data['message'] ?? 'Something went wrong';
+      } else {
+        message = e.message ?? 'Network Error';
+      }
+
+      notifyListeners();
+      return false;
     } catch (e) {
       message = e.toString();
       notifyListeners();
